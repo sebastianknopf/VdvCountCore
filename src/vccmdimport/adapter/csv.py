5bfaae571a6ec931e4ba07ec6c8cce0723430d92
sebastianknopf <43302817+sebastianknopf@users.noreply.github.com>
2025-04-16 07:33:55 +0200

    added basic adapter for CSV skeleton, references #9

---
+++ src/vccmdimport/adapter/csv.py
@@ -0,0 +1,11 @@
+import csv
+
+from vccmdimport.adapter.base import BaseAdapter
+
+class DefaultAdapter(BaseAdapter):
+
+    def __init__(self) -> None:
+        pass
+
+    def process(self, input_directory: str) -> None:
+        pass
