9ba511a5bc75504deea04d2a4e3f6adeb1ecb98a
sebastianknopf <43302817+sebastianknopf@users.noreply.github.com>
2025-05-03 12:11:15 +0200

    implemented and tested fully working archive logic, references #16

--- src/vccvdv457export/__main__.py
+++ src/vccvdv457export/__main__.py
@@ -3,12 +3,15 @@ import logging
 import os
 import time
 
-import vccvdv457export.adapter.s2 as s2
-import vccvdv457export.adapter.s3 as s3
+import vccvdv457export.adapter.s2.default as s2
+import vccvdv457export.adapter.s3.default as s3
 
 from croniter import croniter
 from datetime import datetime
 
+from vcclib.filesystem import directory_contains_files
+from vcclib.filesystem import stage_directory_files
+from vcclib.filesystem import archive_directory_files
 from vccvdv457export.adapter.base import BaseAdapter
 
 def run():
@@ -20,6 +23,25 @@ def run():
         _run_now()
 
 def _run_now():
+
+    # set local varibales
+    input_directory = '/data/input'
+    stage_directory = '/data/stage'
+
+    # check if input directory contains files at all...
+    if not directory_contains_files(input_directory):
+        logging.info(f"Input directory {input_directory} is empty")
+        return
+    else:
+        # take files into staging directory in order to avoid archiving files which were added after starting a converter
+        # remember: the converter may run several minutes!
+        logging.info(f"Staging files in input directory {input_directory} to stage directory {stage_directory}  ...")
+        stage_directory_files(input_directory, stage_directory)
+
+    # check whether all converters were running fine...
+    # set to true at first, in case there's no converter enabled at all to avoid unneccessary 
+    # archive folders
+    any_converter_failed = False
 
     # instantiate and run VDV457-2 conversion
     vdv457_convert_2 = os.getenv('VCC_VDV457_EXPORT_CONVERT_2', 'false').lower()
@@ -34,8 +56,9 @@ def _run_now():
             raise ValueError(f"Unknown VDV457-2 adapter type {adapter_type}!")
         
         try:
-            adapter.process('/data/input', '/data/vdv4572')
+            adapter.process(stage_directory, '/data/vdv4572')
         except Exception as ex:
+            any_converter_failed = True
             if os.getenv('VCC_DEBUG', '0') == '1':
                 logging.exception(ex)
             else:
@@ -54,12 +77,18 @@ def _run_now():
             raise ValueError(f"Unknown VDV457-3 adapter type {adapter_type}!")
 
         try:
-            adapter.process('/data/input', '/data/vdv4573')
+            adapter.process(stage_directory, '/data/vdv4573')
         except Exception as ex:
+            any_converter_failed = True
             if os.getenv('VCC_DEBUG', '0') == '1':
                 logging.exception(ex)
             else:
-                logging.error(str(ex))   
+                logging.error(str(ex))  
+
+    # if all converters ran without error, archive input files; otherwise put them in an error folder
+    # archive shall be created in input directory, so pass this as destination variable
+    logging.info(f"Archiving files in input directory {stage_directory} ...")
+    archive_directory_files(stage_directory, input_directory, any_converter_failed)
 
 @click.group()
 def cli():
