be7dedd2fb69882ae9f4aea1867b3044fa1f9b8d
sebastianknopf <43302817+sebastianknopf@users.noreply.github.com>
2025-05-03 10:52:38 +0200

    added dict2xml function in xml module, references #16

---
+++ src/vcclib/xml.py
@@ -0,0 +1,36 @@
+from lxml import etree
+
+def _build_xml_element(parent, d, attribute_mapping=None):
+    
+    if attribute_mapping is None:
+        attribute_mapping = {}
+
+    if isinstance(d, dict):
+        attrib_keys = attribute_mapping.get(parent.tag, set())
+        attrib = {}
+        content = {}
+
+        for key, value in d.items():
+            if key in attrib_keys:
+                attrib[key] = str(value)
+            else:
+                content[key] = value
+
+        parent.attrib.update(attrib)
+
+        for key, value in content.items():
+            if isinstance(value, list):
+                for item in value:
+                    child = etree.SubElement(parent, key)
+                    _build_xml_element(child, item, attribute_mapping)
+            else:
+                child = etree.SubElement(parent, key)
+                _build_xml_element(child, value, attribute_mapping)
+    else:
+        parent.text = str(d)
+
+def dict2xml(tag, d, attribute_mapping=None):
+    root = etree.Element(tag)
+    _build_xml_element(root, d, attribute_mapping)
+
+    return root
