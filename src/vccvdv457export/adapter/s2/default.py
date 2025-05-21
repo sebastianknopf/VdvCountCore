d4a95e82064128fbdb534b0348ba175754ded2de
mail@sebastian-knopf.de <mail@sebastian-knopf.de>
2025-05-21 22:08:05 +0200

    added basic structure and calls to default VDV457-2 converter, references #16

--- src/vccvdv457export/adapter/s2/default.py
+++ src/vccvdv457export/adapter/s2/default.py
@@ -1,3 +1,5 @@
+import logging
+
 from vcclib.duckdb import DuckDB
 
 from vccvdv457export.adapter.base import BaseAdapter
@@ -9,3 +11,13 @@ class DefaultAdapter(BaseAdapter):
 
     def process(self, ddb: DuckDB, output_directory: str) -> None:
         super().process(ddb, output_directory)
+
+        for (operation_day, trip_id), device_id in ddb.get_primary_indicators().items():
+
+            primary_data = ddb.get_primary_data(operation_day, trip_id, device_id)
+
+            logging.info(primary_data)
+
+            secondary_data = ddb.get_secondary_data(operation_day, trip_id, device_id)
+
+            logging.info(secondary_data)
