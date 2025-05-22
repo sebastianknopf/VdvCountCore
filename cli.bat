27815d75491394e73e0abe65ffb4cf30fd4a7da8
sebastianknopf <mail@sebastian-knopf.de>
2025-05-22 11:31:00 +0200

    fixed spelling mistake in cli script, references #16

--- cli.bat
+++ cli.bat
@@ -25,7 +25,7 @@ if not exist "%envfile%" (
 set ENV_FILE=%envfile%
 
 echo.
-set /p build=Do you want to force a build first (y/N)?
+set /p build=Do you want to force a build first (y/N)? 
 echo.
  
 if /i "%build%"=="y" (
