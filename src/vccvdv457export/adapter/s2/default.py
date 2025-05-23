233a40fa7212e2759cdb61c6667637d2447a95c9
mail@sebastian-knopf.de <mail@sebastian-knopf.de>
2025-05-23 16:42:12 +0200

    created basic function structure in VDV457-2 default adapter, references #16

--- src/vccvdv457export/adapter/s2/default.py
+++ src/vccvdv457export/adapter/s2/default.py
@@ -1,6 +1,11 @@
 import logging
 
+from typing import Dict
+from typing import List
+
+from vcclib.dataclasses import PassengerCountingEvent
 from vcclib.duckdb import DuckDB
+from vcclib.xml import dict2xml
 
 from vccvdv457export.adapter.base import BaseAdapter
 from vccvdv457export.collector import PassengerCountingEventCollector
@@ -12,6 +17,25 @@ class DefaultAdapter(BaseAdapter):
 
     def process(self, ddb: DuckDB, output_directory: str) -> None:
         super().process(ddb, output_directory)
+
+        # extract PCE from loaded data
+        extracted_data: Dict[tuple, List[PassengerCountingEvent]] = self._extract(ddb)
+
+        # transform each operation_day/trip_id combination into final data structure
+        transformed_data: List[str] = list()
+        for (operation_day, trip_id), passenger_counting_events in extracted_data.items():
+            transformed_data.append(
+                self._transform(operation_day, trip_id, passenger_counting_events)
+            )
+
+        # export data finally
+        self._export(transformed_data)
+
+        logging.info(f"Exported {len(transformed_data)} trips total")
+
+    def _extract(self, ddb: DuckDB) -> Dict[tuple, List[PassengerCountingEvent]]:
+
+        results: Dict[tuple, List[PassengerCountingEvent]] = dict()
 
         logging.info("Loading primary indicators")
         for (operation_day, trip_id), primary_device_id in ddb.get_primary_indicators().items():
@@ -34,7 +58,23 @@ class DefaultAdapter(BaseAdapter):
             # call verify method to log every possible invalid dataset
             collector.verify()
 
-            # transform collected PCEs into final structure
+            # return finally generated PCE data
             passenger_counting_events = collector.get_passenger_counting_events()
 
             logging.info(f"Found total {len(passenger_counting_events)} PCEs")
+
+            results[(operation_day, trip_id)] = passenger_counting_events
+        
+        # finally return generated results
+        return results
+    
+    def _transform(self, operation_day: int, trip_id: int, passenger_counting_events: List[PassengerCountingEvent]) -> str:
+
+        result: dict = dict()
+
+        # todo: implementation following here
+
+        return dict2xml(result)
+    
+    def _export(self, transformed_data: List[str]) -> None:
+        pass
