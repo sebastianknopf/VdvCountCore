fe2ce60fe0f67298c9b7414459ea1777176ba840
mail@sebastian-knopf.de <mail@sebastian-knopf.de>
2025-05-26 11:44:11 +0200

    fixed entrypoint.sh for vcc-proxy and update .env file structure to work properly, references #16

--- src/vccproxy/entrypoint.sh
+++ src/vccproxy/entrypoint.sh
@@ -2,9 +2,11 @@
 
 htpasswd -bc /usr/local/apache2/conf/.htpasswd ${VCC_USERNAME} ${VCC_PASSWORD}
 
-if [ \"$VCC_PROXY_SSL_ACTIVE\" = \"true\"]; then
+if [ "$VCC_PROXY_SSL_ACTIVE" = "true" ]; then
+    echo 'Enabling SSL for Proxy'
     echo 'Include /usr/local/apache2/conf/extra/httpd-vcc-proxy-ssl.conf' >> /usr/local/apache2/conf/httpd.conf
 else
+    echo 'Disabling SSL for Proxy'
     echo 'Include /usr/local/apache2/conf/extra/httpd-vcc-proxy.conf' >> /usr/local/apache2/conf/httpd.conf
 fi
 
